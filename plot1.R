0d828499927bfb2ae8a5f68709622873c5a92559
saviosampaio <saviosampaio@gmail.com>
2017-02-15 00:43:17 -0200

    exploratory-data-analysis/course-project-1

--- plot1.R
+++ plot1.R
@@ -1,8 +1,14 @@
 # saviosampaio - 20170215-0019
+# - plot1.R do course-project-1 de exploratory-data-analysis / coursera
+
+# aqui fiz uma melhoria, para realizar o download dos dados direto da fonte
+# - salvei uma copia desse arquivo ZIP dentro do diretorio raiz desse course-project-1
+url <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(url, "household_power_consumption.zip")
 
 # usei a funcao unz para tratar os dados compactados e facilitar o seu compartilhamento e a reproducao desse grafico
 # o comando abaixo vai extrair o arquivo e importa-lo para a tabela "dados", usando informacoes como o separador ";"
-dados <- read.table(unz("./data/household_power_consumption.zip", "household_power_consumption.txt"), header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+dados <- read.table(unz("household_power_consumption.zip", "household_power_consumption.txt"), header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 
 # podemos "filtrar" as linhas (rows) de varias maneiras
 #analisar <- dados[dados$Date %in% c("1/2/2007","2/2/2007") ,]
@@ -16,5 +22,4 @@ hist(as.numeric(analisar$Global_active_power), col="red", main="Global Active Po
 
 # encerramos o device PNG
 dev.off()
-
 
